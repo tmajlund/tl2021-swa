e3cece37d1375ee3dd09e3519fb20845b830f1dc
Therese Berg <70585763+thereseberg@users.noreply.github.com>
2021-09-27 15:46:58 +0200

    added post/get api-functions

---
+++ Shared/ApiResponse.cs
@@ -0,0 +1,9 @@
+namespace BlazorApp.Shared
+{
+    public class ApiResponse
+    {
+        public int NumberOfVoters { get; set; }
+        public int NumberOfYes { get; set; }
+        public int NumberOfNo { get; set; }
+    }
+}
